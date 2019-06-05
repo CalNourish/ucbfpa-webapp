e34fc54d9da4179d965ad9febf1aefb20027c6fa
Wayne Li <wli2@berkeley.edu>
2019-06-04 22:50:44 -0700

    Add a start order javascript

---
+++ public/scripts/start-order.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// var form = document.getElementById('start-order-form');
+
+function saveOrder() {
+    var encryptedStudentId = document.getElementById('encryptedStudentId').value;
+    var studentStatus = document.getElementById('studentStatus').value;
+
+    // TODO: Null checks.
+  
+    var order = {
+        encrypted_student_id: encryptedStudentId,
+        student_status: studentStatus
+    };
+
+    return firebase
+        .database()
+        .ref('/transaction2')
+        .update(order)
+        .catch(function(error) {
+            console.error('Error saving order to /transaction2', error);
+        });
+}
