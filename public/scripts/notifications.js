bdf964aa0d19912f447bd7635631594b95573864
Wayne Li <wli2@berkeley.edu>
2019-06-14 21:27:36 -0700

    Allow to receive notifications

--- public/scripts/notifications.js
+++ public/scripts/notifications.js
@@ -1,5 +1,7 @@
 'use strict';
 
+configureNotifications();
+
 var formElement = document.getElementById('send-notification-form');
 var notifTitleElement = document.getElementById('notifTitle');
 var notifTextElement = document.getElementById('notifText');
@@ -14,27 +16,23 @@ function sendNotification(event) {
     console.log(notifText);
 }
 
+function configureNotifications() {
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+    } else {
+      console.log('Unable to get permission to notify.');
+    }
+  });
+}
+
 formElement.addEventListener('submit', sendNotification);
 
-// function goToCheckout() {
-//     window.location.href = "/checkout";
+// function trash() {
+//   const messaging = firebase.messaging();
+//   messaging.usePublicVapidKey("BOtZcASfobhUcw5aNkd2URdFSeWWlN-mdeYDT5rCsH2ONC5SZ2RcsQhF4gCx7QFjfk1cOHu54Yuymb_W5Mvce_E");
 // }
 
-// function configureNotifications() {
-//     const messaging = firebase.messaging();
-//     messaging.usePublicVapidKey("BOtZcASfobhUcw5aNkd2URdFSeWWlN-mdeYDT5rCsH2ONC5SZ2RcsQhF4gCx7QFjfk1cOHu54Yuymb_W5Mvce_E");
-  
-//     Notification.requestPermission().then((permission) => {
-//       if (permission === 'granted') {
-//         console.log('Notification permission granted.');
-//         // TODO(developer): Retrieve an Instance ID token for use with FCM.
-//         // ...
-//       } else {
-//         console.log('Unable to get permission to notify.');
-//       }
-//     });
-// }
-  
 //   // Get Instance ID token. Initially this makes a network call, once retrieved
 //   // subsequent calls to getToken will return from cache.
 // messaging.getToken().then((currentToken) => {
