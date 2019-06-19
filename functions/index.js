b07dd3315a946c1a147ea186a94c5030037fa4fb
Wayne Li <wli2@berkeley.edu>
2019-06-18 22:45:30 -0700

    Subscribe to topic code

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const request = require('request');
+
+admin.initializeApp();
+
+exports.subscribeToFoodPantry = functions.database.ref('/notificationToken/{notificationTokenUid}')
+    .onWrite(async (change, context) => {
+
+      const notificationTokenUid = context.params.notificationTokenUid;
+
+      request
+        .post(
+	      'https://iid.googleapis.com/iid/v1/' + notificationTokenUid + '/rel/topics/foodPantry',
+	      {
+	      	json: {},
+	      	'auth': {
+              'key': '<API_KEY>'
+            }
+	      },
+	      function (error, response, body) {
+	        if (!error && response.statusCode == 200) {
+	          console.log(body);
+	        }
+	      }
+	    );
+    });
