55419b6758a5fd559d2697a0360ac403845719b3
Wayne Li <wli2@berkeley.edu>
2019-06-18 22:50:07 -0700

    Push error log

--- functions/index.js
+++ functions/index.js
@@ -23,6 +23,8 @@ exports.subscribeToFoodPantry = functions.database.ref('/notificationToken/{noti
 	      function (error, response, body) {
 	        if (!error && response.statusCode == 200) {
 	          console.log(body);
+	        } else {
+	          console.log(error);
 	        }
 	      }
 	    );
