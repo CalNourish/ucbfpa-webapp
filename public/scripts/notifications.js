1b809913eb8747bb9fc18cff2fe9049e34bbfb93
Samantha Lai <samanthalai@Samanthas-MacBook-Pro.local>
2019-06-21 01:00:54 -0700

    push -> update

--- public/scripts/notifications.js
+++ public/scripts/notifications.js
@@ -40,17 +40,19 @@ function sendNotification(event) {
 
 function sendTokenToServer(generatedToken) {
   event.preventDefault();
+  // null check
+  // var token = generatedToken;
+  var token = {};
+  token[generatedToken] = ''; 
 
-  var token = generatedToken; 
-
-  var notificationToken = {
-      token: token,
-  };
+  // var notificationToken = {
+  //     token: token,
+  // };
 
   firebase
       .database()
       .ref('/notificationToken')
-      .push(notificationToken)
+      .update(token)
       // .then(function() {
       //     // goToCheckout();
       //     notifTitleElement.value = '';
@@ -73,9 +75,6 @@ function configureNotifications() {
         if (currentToken) {
           sendTokenToServer(currentToken);
           console.log(currentToken);
-          messaging.onMessage((payload) => {
-            console.log('Message received. ', payload);
-          });
         } else {
           console.log('No Instance ID token available. Request permission to generate one.');
         }
@@ -90,3 +89,43 @@ function configureNotifications() {
 }
 
 formElement.addEventListener('submit', sendNotification);
+
+// function trash() {
+//   const messaging = firebase.messaging();
+//   messaging.usePublicVapidKey("BOtZcASfobhUcw5aNkd2URdFSeWWlN-mdeYDT5rCsH2ONC5SZ2RcsQhF4gCx7QFjfk1cOHu54Yuymb_W5Mvce_E");
+// }
+
+//   // Get Instance ID token. Initially this makes a network call, once retrieved
+//   // subsequent calls to getToken will return from cache.
+// messaging.getToken().then((currentToken) => {
+//     if (currentToken) {
+//       sendTokenToServer(currentToken);
+//       updateUIForPushEnabled(currentToken);
+//     } else {
+//       // Show permission request.
+//       console.log('No Instance ID token available. Request permission to generate one.');
+//       // Show permission UI.
+//       updateUIForPushPermissionRequired();
+//       setTokenSentToServer(false);
+//     }
+//   }).catch((err) => {
+//     console.log('An error occurred while retrieving token. ', err);
+//     showToken('Error retrieving Instance ID token. ', err);
+//     setTokenSentToServer(false);
+// });
+  
+//   // Callback fired if Instance ID token is updated.
+// messaging.onTokenRefresh(() => {
+//     messaging.getToken().then((refreshedToken) => {
+//       console.log('Token refreshed.');
+//       // Indicate that the new Instance ID token has not yet been sent to the
+//       // app server.
+//       setTokenSentToServer(false);
+//       // Send Instance ID token to app server.
+//       sendTokenToServer(refreshedToken);
+//       // ...
+//     }).catch((err) => {
+//       console.log('Unable to retrieve refreshed token ', err);
+//       showToken('Unable to retrieve refreshed token ', err);
+//     });
+// });
