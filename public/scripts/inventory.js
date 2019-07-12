d0bccf447e4765d7a82276928c874c6c057ad2bf
jschnapper <jschnap717@gmail.com>
2019-07-11 22:47:50 -0400

    watching for changes

--- public/scripts/inventory.js
+++ public/scripts/inventory.js
@@ -1,23 +1,29 @@
 'use strict';
 
 $(document).ready(function() {
+  // list for appending to DOM
   let items = [];
   
+  // connect inventory
   const REF = firebase.database().ref('/inventory')
 
-    REF.on("value", snapshot => {
+  // initial data 
+  REF.once("value", snapshot => {
+    let res = snapshot.val()
+    console.log(res)
+    for (let item in res) {
+      let currentItem = res[item]
+      console.log(currentItem.barcode)
+      items.push(`<div class='card'><img class='card-img-top' src='https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80' alt='Card image cap'><div class='card-body'><h4 class='item-name'>${currentItem.itemName}</h4><p class='card-text item-count' data-itemid='${currentItem.barcode}'>${currentItem.count}</p></div></div>`)
+    }
+    $("#inventory-items").append(items)
+    })
+
+    // watch for data changes while page is open
+    REF.on("child_changed", snapshot => {
       let res = snapshot.val()
-      console.log(res)
-      for (let item in res) {
-        let currentItem = res[item]
-        items.push(`<div class='card'><img class='card-img-top' src='https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80' alt='Card image cap'><div class='card-body'><h4 class='item-name'>${currentItem.itemName}</h4><p class='card-text item-count'>${currentItem.count}</p></div></div>`)
-      }
-      $("#inventory-items").append(items)
-      })
-
-
-    // REF.on("child_changed", snapshot => {
-    //   let res = snapshot.val()
-    //   console.log(res)
-    // })
+      let item = document.querySelector(`[data-itemid='${res.barcode}']`)
+      item.textContent = res.count;
+      console.log(item)
+    })
   });
