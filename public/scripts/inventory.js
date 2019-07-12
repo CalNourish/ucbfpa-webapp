1f2d17123a3f2b145fae1bd269c8a3a3e6393cd9
jschnapper <jschnap717@gmail.com>
2019-07-11 22:57:55 -0400

    fixed watch

--- public/scripts/inventory.js
+++ public/scripts/inventory.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
   // initial data 
   REF.once("value", snapshot => {
     let res = snapshot.val()
-    console.log(res)
     for (let item in res) {
       let currentItem = res[item]
       console.log(currentItem.barcode)
       items.push(`<div class='card'><img class='card-img-top' src='https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80' alt='Card image cap'><div class='card-body'><h4 class='item-name'>${currentItem.itemName}</h4><p class='card-text item-count' data-itemid='${currentItem.barcode}'>${currentItem.count}</p></div></div>`)
     }
+    // append to dom
     $("#inventory-items").append(items)
     })
 
@@ -24,6 +24,5 @@ $(document).ready(function() {
       let res = snapshot.val()
       let item = document.querySelector(`[data-itemid='${res.barcode}']`)
       item.textContent = res.count;
-      console.log(item)
     })
   });
