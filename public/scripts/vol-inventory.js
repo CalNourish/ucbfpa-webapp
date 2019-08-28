6c371ff7cdc7116e1c4ce781d3f36809c39401d5
jschnapper <jschnap717@gmail.com>
2019-08-27 22:38:21 -0400

    set up toasts for inventory

--- public/scripts/vol-inventory.js
+++ public/scripts/vol-inventory.js
@@ -142,3 +142,21 @@ document.onclick = function(e){
     return;
   }
 };
+
+// Toast options
+toastr.options = {
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "showDuration": "200",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "2000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
